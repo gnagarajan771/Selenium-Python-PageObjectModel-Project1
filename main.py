09d3a6235f42d875fdd5c1b3ed961db0ecb6f06b
gnagarajan771 <141901445+gnagarajan771@users.noreply.github.com>
2023-12-12 17:27:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 
 # Check Python version
-if not (sys.version_info.major == 3 and sys.version_info.minor >= 6):
-    print("This script requires Python 3.6 or higher!")
+if not (sys.version_info.major == 3 and sys.version_info.minor >= 10):
+    print("This script requires Python 3.10 or higher!")
     print("You are using Python {}.{}.".format(sys.version_info.major, sys.version_info.minor))
     sys.exit(1)
