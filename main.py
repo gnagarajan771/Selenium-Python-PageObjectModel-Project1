84c59d100fea819b49e12a92fe6939f5d3d1d278
gnagarajan771 <141901445+gnagarajan771@users.noreply.github.com>
2023-12-12 17:26:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Check Python version
+if not (sys.version_info.major == 3 and sys.version_info.minor >= 6):
+    print("This script requires Python 3.6 or higher!")
+    print("You are using Python {}.{}.".format(sys.version_info.major, sys.version_info.minor))
+    sys.exit(1)
